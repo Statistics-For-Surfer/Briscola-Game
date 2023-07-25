63fe4b11efa0183e0723a6ea3288d3247d1082ca
Matteo Candi <114286004+Matteo-Candi@users.noreply.github.com>
2023-07-25 17:28:25 +0200

    Update plots.r

--- plots.r
+++ plots.r
@@ -5,7 +5,7 @@ colnames(decks_10) <- c('Wins', 'Loss', 'Ties')
 rownames(decks_10) <- c('Random', 'Greedy')
 decks_10
 
-decks_20 <- data.frame(matrix(c(), nrow=2, ncol = 3, byrow = T))
+decks_20 <- data.frame(matrix(c(783, 207, 10, 799, 201, 0), nrow=2, ncol = 3, byrow = T))
 colnames(decks_20) <- c('Wins', 'Loss', 'Ties')
 rownames(decks_20) <- c('Random', 'Greedy')
 decks_20
@@ -39,7 +39,7 @@ library(ggplot2)
 
 Decks <- c(rep("10", 2), rep("20", 2), rep("50", 2), rep('100', 2), rep("200", 2), rep('all', 2))
 Opponent <- rep(c("Random", "Greedy"), 6)
-Win <- c(0.773, 0.937, 0, 0, 0.658, 0.604, 0.632, 0.494, 0.555, 0.414, 0.513, 0.317)
+Win <- c(0.773, 0.937, 0.783, 0.799, 0.658, 0.604, 0.632, 0.494, 0.555, 0.414, 0.513, 0.317)
 data <- data.frame(Decks, Opponent, Win)
 data$Decks <- factor(data$Decks, levels = c("10", "20", "50", "100", "200", "all"))
 data$Opponent <- factor(data$Opponent, levels = c("Random", "Greedy"))
